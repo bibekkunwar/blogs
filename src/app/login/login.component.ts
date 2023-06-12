95be1f3905b060d9779c11dc176cbe72b5d9d234
bibekkunwar <bibekkunwar0@gmail.com>
2023-06-12 14:18:09 +0545

    all added 2nd

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { DemoService } from '../demo.service';
+// import { BlogList } from '../data';
+import {
+  HttpHeaders,
+  HttpClient,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +15,24 @@ import { DemoService } from '../demo.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
- /* `form!: FormGroup;` is declaring a property named `form` of type `FormGroup`. The exclamation mark
- (`!`) is a non-null assertion operator, which tells TypeScript that the `form` property will be
- initialized at some point before it is used, even though it is currently undefined. This is useful
- when working with Angular templates, which may access component properties before they are fully
- initialized. */
+  /* `form!: FormGroup;` is declaring a property named `form` of type `FormGroup`. The exclamation mark
+  (`!`) is a non-null assertion operator, which tells TypeScript that the `form` property will be
+  initialized at some point before it is used, even though it is currently undefined. This is useful
+  when working with Angular templates, which may access component properties before they are fully
+  initialized. */
   form!: FormGroup;
 
-  constructor(private _apiService: DemoService) {}
+  accessToken: any = '';
+  refreshToken: any = '';
+  // data: BlogList[] = [];
 
+  constructor(private _apiService: DemoService, private router: Router) {}
+
+ /* `ngOnInit()` is a lifecycle hook in Angular that is called after the component has been
+ initialized. In this code, it is used to call the `createForm()` method, which creates a new
+ instance of `FormGroup` and assigns it to the `form` property of the component. The `FormGroup` is
+ used to create a form with two form controls, `username` and `password`, which are initialized with
+ empty string values. This form is used to capture user input for the login process. */
   ngOnInit(): void {
     this.createForm();
   }
@@ -28,26 +44,25 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  token = '';
 
+/**
+ * The login function sends a login request to an API service and handles the response, storing the
+ * authentication token in local storage and navigating to the user blog page if successful, or
+ * displaying an error message if the request fails with a 401 status code.
+ */
   login() {
-    console.log(this.form.value);
-
-    localStorage.setItem('token', JSON.stringify(this.token));
-    /* This code is making a HTTP POST request to the API service's `login` endpoint with the form data
-    as the request body. The `subscribe` method is used to handle the response from the server. When
-    the response is received, it logs the response to the console and sets the token in the local
-    storage. It also has a commented out line that would display an alert message if the login was
-    successful. */
-    this._apiService.login(this.form.value).subscribe((res) => {
-      console.log('login response', res);
-      localStorage.setItem('token', JSON.stringify(this.token));
-      // alert("Logged in Successfully !")
+    this._apiService.login(this.form.value).subscribe({
+      next: (res) => {
+        localStorage.setItem('auth_token', JSON.stringify(res));
+        alert("Logged in successfully");
+        this.router.navigate(['/userBlog']);
+      },
+      error: (error: HttpErrorResponse) => {
+          if(error.status===401 || error){
+            alert(error.error.detail || "Enter valid details")
+          }
+      },
     });
-  }
 
-  getList() {
-    this._apiService.getBlogList().subscribe((res) => console.log(res));
-    console.log('api access',this.getList);
   }
 }
